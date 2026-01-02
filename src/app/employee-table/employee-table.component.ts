5d9c3eb9525709a213c1d9e6cdbaa3aa819bcfd7
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-02 19:02:39 +0200

    Showing the employee data in a table

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee';
 import { EmployeeService } from '../employee.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'employee-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css',
 })
@@ -17,7 +18,6 @@ export class EmployeeTableComponent {
   ngOnInit() {
     this.employeeService.getEmployees().subscribe((data: Employee[]) => {
       this.employees = data;
-      console.log(data);
     });
   }
 }
