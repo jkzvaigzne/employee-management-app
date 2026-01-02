01119caa5be8bab270fea7035b33d816c3817347
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-02 18:54:15 +0200

    Loading employees from the api

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'employee-table',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css',
 })
-export class EmployeeTableComponent {}
+export class EmployeeTableComponent {
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.employeeService.getEmployees().subscribe((data: Employee[]) => {
+      this.employees = data;
+      console.log(data);
+    });
+  }
+}
