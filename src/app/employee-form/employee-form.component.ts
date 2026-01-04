9e8530fcd17ba16bf149873f972b631ba897a5c7
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2026-01-04 13:47:46 +0200

    Handling observable errors and showing error messages with conditional rendering

--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Employee } from '../models/employee';
 import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css',
 })
@@ -20,11 +22,22 @@ export class EmployeeFormComponent {
     position: '',
   };
 
-  constructor(private employeeService: EmployeeService) {}
+  errorMessage: string = '';
+
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
   onSubmit(): void {
-    this.employeeService
-      .createEmployee(this.employee)
-      .subscribe((result) => console.log(result));
+    this.employeeService.createEmployee(this.employee).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = `Error occured (${err.status})`;
+      },
+    });
   }
 }
